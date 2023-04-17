a3f2632c214e30a75884e219f929b90769e8e743
Metranohm <andrewwink@gmail.com>
2023-04-17 17:13:12 -0500

    implement Joi

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
@@ -41,7 +42,19 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+        const campgroundSchema = Joi.object({
+            campground: Joi.object({
+                title: Joi.string().required(),
+                price: Joi.number().required().min(0),
+            }).required()
+        })
+        const { error } = campgroundSchema.validate(req.body);
+        if(error) {
+            const msg = error.details.map(el => el.message).join(',')
+            throw new ExpressError(msg, 400)
+        }
+        console.log(result)
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`)
