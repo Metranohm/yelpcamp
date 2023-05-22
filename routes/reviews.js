de41e0a9c22c8e43fa68dfde55e9a9156f3a5de7
Metranohm <andrewwink@gmail.com>
2023-05-22 14:13:22 -0500

    refactor users routes into controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,7 @@ const Campground = require('../models/campground');
 const Review = require('../models/review');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
+const reviews = require('../controllers/reviews');
 // const { reviewSchema } = require('../schemas.js');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const { moveCursor } = require('readline');
