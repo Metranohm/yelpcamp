16dcbd804ab70cec1a8da76be325f3caff1fdbcd
Metranohm <andrewwink@gmail.com>
2023-05-22 17:49:22 -0500

    cloudinary config

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,20 +3,21 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const Campground = require('../models/campground');
 const catchAsync = require('../utils/catchAsync');
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
-
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer  = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
 
 router.route('/') 
     .get(catchAsync(campgrounds.index))
     // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.single('image'), (req, res) => {
+    .post(upload.array('image'), (req, res) => {
         console.log(req.body, req.files)
         res.send('It worked!')
-    })
+    });
 
-router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
